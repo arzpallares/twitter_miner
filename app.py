8c143493d5d666c28547fb707fe3dc8f24ba0b61
= <=>
2022-12-04 20:48:03 -0300

    Moving user and tweets to models

--- app.py
+++ app.py
@@ -2,8 +2,6 @@
 # Base imports
 import os
 import typing
-from dataclasses import dataclass
-from abc import ABC, abstractmethod
 
 # Data Format imports
 import json
@@ -15,6 +13,9 @@ from dotenv import load_dotenv
 import tweepy
 from tweepy.errors import Unauthorized
 
+# Model import
+from models import *
+
 # Load Environment variables
 load_dotenv()
 
@@ -25,80 +26,13 @@ ACCESS_TOKEN = os.getenv("ACCESS_TOKEN") or ""
 ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET") or ""
 
 
-class TwitterObj(ABC):
-    """Abstract Object for common Twitter Object Functions"""
-    @abstractmethod
-    def cast_to_dict(self) -> typing.Dict:
-        pass
-
-
-@dataclass
-class User(TwitterObj):
-    """Object representing a Twitter Account"""
-    id: int
-    name: str
-    url: str
-    location: str
-    total_followers: int
-    total_friends: int
-    total_posts: int
-    tweets: typing.Optional[typing.List] = None
-
-    def set_posts(self, tweets:typing.List=[]) -> None:
-        """Set User's tweets"""
-        self.tweets = tweets
-
-    def cast_to_dict(self) -> typing.Dict:
-        """Convert User data into a dictionary"""
-
-        return {
-            'id': self.id,
-            'name': self.name,
-            'url': self.url,
-            'location': self.location,
-            'total_followers': self.total_followers,
-            'total_friends': self.total_friends,
-            'total_posts': self.total_posts,
-            'tweets': self.tweets,
-        }
-
-
-@dataclass
-class Tweet(TwitterObj):
-    """Object representing a Tweet"""
-    id: str
-    user_name: str
-    created_at: str
-    content: str
-    source: str
-    retweet_count: int
-    favorite_count: int
-    coordinates: typing.Dict
-    entities: typing.Dict
-
-    def cast_to_dict(self) -> typing.Dict:
-        """Convert Tweet data into a dictionary"""
-
-        return {
-            'id': self.id,
-            'username': self.user_name,
-            'created_at': self.created_at,
-            'content': self.content,
-            'source': self.source,
-            'retweet_count': self.retweet_count,
-            'favorite_count': self.favorite_count,
-            'coordinates': self.coordinates if self.coordinates else {},
-            'entities': self.entities if self.entities else {},
-        }
-
-
 class ETLHandler:
     """Handle ETL processes"""
 
     def __init__(self, api:tweepy.API) -> None:
         self.api = api
 
-    def get_user_data(self, username:str = "twitter") -> User:
+    def get_user_data(self, username:str = "twitter") -> models.User:
         """Get User account information"""
         target_user = self.api.get_user(screen_name=username)
 
@@ -108,7 +42,7 @@ class ETLHandler:
         followers = target_user.followers_count
         friends = target_user.friends_count
 
-        user = User(
+        user = models.User(
             id=id,
             name=name,
             url=target_user.url,
@@ -119,10 +53,10 @@ class ETLHandler:
 
         return user
 
-    def get_tweet_data(self, status:tweepy.models.Status) -> Tweet:
+    def get_tweet_data(self, status:tweepy.models.Status) -> models.Tweet:
         """Transform Status model into a Tweet object"""
         
-        return Tweet(
+        return models.Tweet(
             id=status.id_str,
             user_name=status.user.screen_name,
             created_at=status.created_at.strftime("%b %d, %Y %H:%M:%S %Z%z"),
@@ -134,7 +68,7 @@ class ETLHandler:
             entities=status.entities,
         )
 
-    def get_user_timeline(self, user:User, count:int=20) -> typing.List:
+    def get_user_timeline(self, user:models.User, count:int=20) -> typing.List:
         """Get User Timeline Posts"""
         return self.api.user_timeline(user_id=user.id, count=count)
 
---
+++ models/models.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# Base imports
+import typing
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+
+class TwitterObj(ABC):
+    """Abstract Object for common Twitter Object Functions"""
+    @abstractmethod
+    def cast_to_dict(self) -> typing.Dict:
+        pass
+
+
+@dataclass
+class User(TwitterObj):
+    """Object representing a Twitter Account"""
+    id: int
+    name: str
+    url: str
+    location: str
+    total_followers: int
+    total_friends: int
+    total_posts: int
+    tweets: typing.Optional[typing.List] = None
+
+    def set_posts(self, tweets:typing.List=[]) -> None:
+        """Set User's tweets"""
+        self.tweets = tweets
+
+    def cast_to_dict(self) -> typing.Dict:
+        """Convert User data into a dictionary"""
+
+        return {
+            'id': self.id,
+            'name': self.name,
+            'url': self.url,
+            'location': self.location,
+            'total_followers': self.total_followers,
+            'total_friends': self.total_friends,
+            'total_posts': self.total_posts,
+            'tweets': self.tweets,
+        }
+
+
+@dataclass
+class Tweet(TwitterObj):
+    """Object representing a Tweet"""
+    id: str
+    user_name: str
+    created_at: str
+    content: str
+    source: str
+    retweet_count: int
+    favorite_count: int
+    coordinates: typing.Dict
+    entities: typing.Dict
+
+    def cast_to_dict(self) -> typing.Dict:
+        """Convert Tweet data into a dictionary"""
+
+        return {
+            'id': self.id,
+            'username': self.user_name,
+            'created_at': self.created_at,
+            'content': self.content,
+            'source': self.source,
+            'retweet_count': self.retweet_count,
+            'favorite_count': self.favorite_count,
+            'coordinates': self.coordinates if self.coordinates else {},
+            'entities': self.entities if self.entities else {},
+        }
